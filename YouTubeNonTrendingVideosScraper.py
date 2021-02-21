36ffdc80c553612b652a891b2d23ccac2f22f40a
Albos Hajdari <albos.hajdari@hotmail.com>
2021-02-21 17:38:56 +0100

    notTrendingFetching done

--- YouTubeNonTrendingVideosScraper.py
+++ YouTubeNonTrendingVideosScraper.py
@@ -7,7 +7,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-#driver = webdriver.Firefox()
+driver = webdriver.Firefox()
+
+df = pd.DataFrame(columns = ['id', 'published_At', 'videot_title', 'description', 'thumbnail_url',
+                             'channel_Title', 'category_Id', 'trending', 'date_registered'])
+
 
 def driver_get_and_scroll(country):
     driver.get("https://www.youtube.com/?gl="+country)
@@ -39,27 +43,31 @@ def get_IDs():
                 attribute = attribute.replace('https://www.youtube.com/watch?v=', '')  #get only the IDs
                 if attribute!="None":
                     IDs.append(attribute)
+    print("IDS = " + str(IDs))
     return IDs
 
 def get_string_param_IDs(IDs):
+    print("Start of get_string_param_IDs")
     total = len(IDs)
     groupsOf50 = int(total/50)
     for g in range(0,groupsOf50):
         splitInGroupsOf50(g*50,g*50+50)
     splitInGroupsOf50(groupsOf50*50,total)
+    print("End of get_string_param_IDs")
     #return stringParamIDs
 
 def splitInGroupsOf50(beginning, end):
+    print("Start of splitInGroupsOf50")
     stringParamIDs = ""
     for i in range(beginning,end):
         stringParamIDs = stringParamIDs+str(IDs[i]) +","
     stringParamIDs = stringParamIDs[:-1]
-    print("Size = " + str(len(stringParamIDs)))
-    print("Grupa = " + stringParamIDs)
-    #getData(stringParamIDs)
-    #addData(getData)
+    data = getData(stringParamIDs,apiKey)
+    addData(data)
+    print("End of splitInGroupsOf50")
 
 def getData(stringParamIDs, apiKey):
+    print("Start of getData")
     # Builds the URL and requests the JSON from it
     request_url = f"https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id={stringParamIDs}&key={apiKey}"
     request = requests.get(request_url)
@@ -67,30 +75,33 @@ def getData(stringParamIDs, apiKey):
         print("Temp-Banned due to excess requests, please wait and continue later")
         sys.exit()
     items = request.json().get('items')
+    print("ITEMS = " + str(items))
+    print("End of getData")
     return items
-    #return data
 
-if __name__ == "__main__":
-    #countriesList = ["AU"]#, "CA", "IE", "JM", "MT", "NZ", "GB", "US"]
-    #for country in countriesList:
-    #    print("COUNTRY = " +country)
-    #    driver_get_and_scroll(country)
-    #    IDs = get_IDs()
-    #    #del IDs[-1] #the attribute of the last element is NONE because it doesn't belong to a video item/element, so I removed it
-    #    get_string_param_IDs(IDs)
-    stringParamIDs = "y_bwKW6V1lw,5qap5aO4i9A"
-    apiKey = "AIzaSyCWhAzyPoNV74327R60On43KOEckztmUpI"
-    data = getData(stringParamIDs,apiKey)
-    df = pd.DataFrame(columns = ['id', 'publishedAt', 'videot_title', 'description',
-                                 'thumbnail_url', 'channelTitle', 'categoryId'])
+def addData(data):
+    print("Start of addData")
+    global df
     for line in data:
         df = df.append({'id' : line['id'], 
-                        'publishedAt' : line['snippet']['publishedAt'], 
+                        'published_At' : line['snippet']['publishedAt'], 
                         'videot_title' : line['snippet']['title'],  
                         'description' : line['snippet']['description'],  
-                        'thumbnail_url' : line['snippet']['thumbnails']['maxres']['url'],  
-                        'channelTitle' : line['snippet']['channelTitle'],  
-                        'categoryId' : line['snippet']['categoryId']},
+                        'thumbnail_url' : line['snippet']['thumbnails']['default']['url'],  
+                        'channel_Title' : line['snippet']['channelTitle'],  
+                        'category_Id' : line['snippet']['categoryId'],
+                        'trending' : 'false',
+                        'date_registered' : time.strftime("%d.%m.%Y")},
                 ignore_index = True)
-    print(df)
-    df.to_csv('file_name.csv')
+    print("End of addData")
+
+if __name__ == "__main__":
+    apiKey = ""
+    countriesList = ["AU"]#, "CA", "IE", "JM", "MT", "NZ", "GB", "US"]
+    for country in countriesList:
+        print("COUNTRY = " +country)
+        driver_get_and_scroll(country)
+        IDs = get_IDs()
+        #del IDs[-1] #the attribute of the last element is NONE because it doesn't belong to a video item/element, so I removed it
+        get_string_param_IDs(IDs)
+        df.to_csv(country+"_"+time.strftime("%d.%m.%Y")+".csv")
