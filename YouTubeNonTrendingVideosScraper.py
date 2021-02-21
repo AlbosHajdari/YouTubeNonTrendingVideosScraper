706d10a3836fc392e2e89ced5c5f6ce80f126ed3
Albos Hajdari <albos.hajdari@hotmail.com>
2021-02-21 12:32:52 +0100

    update

--- YouTubeNonTrendingVideosScraper.py
+++ YouTubeNonTrendingVideosScraper.py
@@ -1,4 +1,4 @@
-import time
+import time, clipboard
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import pandas as pd 
@@ -37,17 +37,45 @@ def get_IDs():
     for i in user_data:
                 attribute = str(i.get_attribute('href'))
                 attribute = attribute.replace('https://www.youtube.com/watch?v=', '')  #get only the IDs
-                IDs.append(attribute)
+                if attribute!="None":
+                    IDs.append(attribute)
     return IDs
 
-if __name__ == "__main__":
-    driver_get_and_scroll("US")
-    IDs = get_IDs()
-    del IDs[-1] #the attribute of the last element is NONE because it doesn't belong to a video item/element, so I removed it
-    
-    
-    i=1
+def get_string_param_IDs(IDs):
+    stringParamIDs = ""
     for id in IDs:
-        print(i)
-        print(id)
-        i = i+1
+        stringParamIDs = stringParamIDs + str(id) + ","
+    stringParamIDs = stringParamIDs[:-1]
+    return stringParamIDs
+
+if __name__ == "__main__":
+    countriesList = ["AU"]#, "CA", "IE", "JM", "MT", "NZ", "GB", "US"]
+    for country in countriesList:
+        print("COUNTRY = " +country)
+        driver_get_and_scroll(country)
+        IDs = get_IDs()
+        del IDs[-1] #the attribute of the last element is NONE because it doesn't belong to a video item/element, so I removed it
+        total = len(IDs)
+        groupsOf50 = int(total/50)
+        for g in range(0,groupsOf50):
+            stringParamIDs = ""
+            for i in range(g*50,g*50+50):
+                stringParamIDs = stringParamIDs+str(IDs[i]) +","
+            stringParamIDs = stringParamIDs[:-1]
+            print("Size = " + str(len(stringParamIDs)))
+            print("Grupa = " + stringParamIDs)
+            #getData(stringParamIDs)
+            #addData(getData)
+        stringParamIDs = ""
+        for i in range(groupsOf50*50,total):
+            stringParamIDs = stringParamIDs+str(IDs[i]) +","
+        stringParamIDs = stringParamIDs[:-1]
+        print("Size = " + str(len(stringParamIDs)))
+        print("Grupa = " + stringParamIDs)
+        #getData(stringParamIDs)
+        #addData(getData)
+
+        #stringParamIDs = get_string_param_IDs(IDs)
+        #print (stringParamIDs)
+        #clipboard.copy(stringParamIDs)
+
